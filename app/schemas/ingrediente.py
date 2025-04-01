42f8c328c6e53b5f09998ee765c5b2d5c739dad1
Abraham <abraham_soto10@outlook.com>
2025-04-01 15:41:50 -0700

    productmanagment

--- app/schemas/ingrediente.py
+++ app/schemas/ingrediente.py
@@ -6,7 +6,9 @@ class IngredienteCreate(BaseModel):
     nombre: str
     unidad: str  # Ej: "g", "ml", "piezas"
     es_producto_tienda: bool = False
+    precio: Optional[float] = None
     enlace_producto: Optional[str] = None
+    costo_extra: Optional[float] = Field(0, ge=0, example=50.0)
 
 class IngredienteOut(BaseModel):
     id: str = Field(..., alias="_id")
